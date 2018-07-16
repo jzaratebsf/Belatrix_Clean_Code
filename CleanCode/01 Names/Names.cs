4c2b816819ced7a92daf445a465b79d0079caee9
John Zarate <jzarate@belatrixsf.com>
2018-07-16 17:41:04 -0500

    Refactoring_ClassName

--- CleanCode/01 Names/Names.cs
+++ CleanCode/01 Names/Names.cs
@@ -1,17 +1,17 @@
 ﻿using System.Drawing;
 
-namespace CleanCode.Names
+namespace CleanCode.GraphicEditor
 {
-    public class Names
+    public class ImageTypeEditor
     {
-        public Bitmap Method1(string n)
+        public Bitmap BitmapDrawer(string filename)
         {
-            var b = new Bitmap(n);
-            var g = Graphics.FromImage(b);
-            g.DrawString("a", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 0));
-            g.DrawString("b", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 20));
-            g.DrawString("c", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 30));
-            return b;
+            var bitmap = new Bitmap(filename);
+            var graphic = Graphics.FromImage(bitmap);
+            graphic.DrawString("a", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 0));
+            graphic.DrawString("b", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 20));
+            graphic.DrawString("c", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 30));
+            return bitmap;
         }
     }
 }
