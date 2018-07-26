69fd6d966d47bd0b9b568b7cad4079b7de7ff2b3
John Zarate <jzarate@belatrixsf.com>
2018-07-26 10:06:22 -0500

    Refactoring_employee

--- SOLID/02 OpenClosed/Employee.cs
+++ SOLID/02 OpenClosed/Employee.cs
@@ -1,30 +1,35 @@
-﻿namespace SOLID._02_OpenClosed
+﻿using SOLID._02_OpenClosed;
+
+namespace SOLID._02_OpenClosed
 {
-    public class Employee
+    public class  Employee
     {
         public int ID { get; set; }
         public string EmployeeType { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; }      
+                
+    }    
 
-             
+    public class newEmployee : Employee
+    {
+        private Employee worker = new Employee();
 
-        public decimal CalculateBonus(decimal salary)
+        void createEmployee(Employee worker, int id, string name, string type)
+        {
+            worker.ID = id;
+            worker.Name = name;
+            worker.EmployeeType = type;
+        }
+    }
+
+    public class CalculateBonusEmployee : Employee
+    {
+        decimal CalculateBonus(decimal salary)
         {
             if (this.EmployeeType == "Permanent")
                 return salary * .1M;
             else
                 return salary * .05M;
         }
-    }
-
-    public interface IEmployeeUpdate
-    {
-        EmployeeUpdate();
-    }
-    public void EmployeeUpdate(int id, string name, string type)
-    {
-        this.ID = id;
-        this.Name = name;
-        this.EmployeeType = type;
     }
 }
